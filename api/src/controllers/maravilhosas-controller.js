17ef6ff59e09d18a92b58db040b08720c0e5c4ad
Lílian Teixeira <lilianhteixeira@gmail.com>
2020-10-26 17:23:11 -0300

    Projeto 2 completo.

--- api/src/controllers/maravilhosas-controller.js
+++ api/src/controllers/maravilhosas-controller.js
@@ -1,11 +1,63 @@
-//Nomes dos métodos para implementação:
+const { request, response } = require("express");
+const mulheres = require("../models/maravilhosas-models");
 
+//Nomes dos métodos para implementação:
 //getMaravilhosas
+const getMaravilhosas = (request, response) => {
+    getMulheres = mulheres.selectAllData(); //ñ esquecer de colocar os ()depois do selectAllData, pq ele é uma função
+    response.status(200).send(getMulheres);
+}
 
 //getMaravilhosaById
+//request e response são responsabilidades do controller
+const getMaravilhosaById = (request, response) => {
+
+    const id = request.params.id;
+    const mulheresById = mulheres.selectDataById(id);
+
+    if(mulheresById){
+        response.status(200).send(mulheresById);
+    } else{
+        response.status(404).send("Mulher maravilhosa não encontrada.");
+    }
+}
 
 //addMaravilhosa 
-
+const addMaravilhosa = (request, response) => {
+    
+    const {error} = mulheres.insertData(request.body);
+    if(error === null) {
+        response.status(201).json("Mulher maravilhosa adicionada com sucesso.");
+    } else {
+        response.status(400).json({"Não foi possível adicionar a mulher maravilhosa.": error.message})
+    }
+}
 //updateMaravilhosa 
+const updateMaravilhosa = (request, response) => {
+
+    const {error, data} = mulheres.updateData(request.params.id, request.body  )
+     if(error=== null) {
+         response.status(201).send(data)
+    } else {
+        response.status(404).json({"Não foi possivel atualizar a mulher maravilhosa.": error.message})
+    }
+}
 
 //deleteMaravilhosa
+const deleteMaravilhosa = (request, response) => {
+    
+    const {error} = mulheres.deleteData(request.params.id)
+    if(error===null) {
+        response.status(204).send("Mulher maravilhosa removida com sucesso.")
+    } else {
+        response.status(404).json({"Não foi possivel remover a mulher maravilhosa.": error.message})
+    }
+}
+
+module.exports = {
+    getMaravilhosas,
+    getMaravilhosaById,
+    addMaravilhosa,
+    updateMaravilhosa,
+    deleteMaravilhosa
+}
--- api/src/models/maravilhosas-models.js
+++ api/src/models/maravilhosas-models.js
@@ -1,9 +1,94 @@
-// selectAllData
+const { request, response } = require("../app");
+const fs = require("fs")
+//não pode ser const pq os dados vão ser alterados
+let dados = require("../data/data.json");
+
+// selectAllData, ñ precisa de request, response pq vão vir todos os dados
+const selectAllData = () => {
+    return dados;
+}
 
 //selectDataById
+const selectDataById = (id) => {
+
+    const mulheresID = id;
+    const dadoEncontrado = dados.find(item => item.id == mulheresID);
+    if(dadoEncontrado){
+        return {data: dadoEncontrado}
+    }else{
+        return {error: {message: "Mulher maravilhosa não encontrada na base de dados."}, data: null}
+    }
+    
+}
+
 
 //insertData
+const insertData = (novaMaravilhosa) => {
+    
+    const maravilhosaFound = dados.find(maravilhosa => maravilhosa.name == novaMaravilhosa.name);
+    
+    if (!novaMaravilhosa.id) {
+        novaMaravilhosa.id = Math.random().toString(36).substr(-8);
+    }
+ 
+    if(maravilhosaFound) {
+       return {error: {message: "Registro duplicado."}} 
+    } else {
+        fs.writeFileSync('./src/data/data.json', JSON.stringify([...dados, novaMaravilhosa]), 'utf8', (err) => {
+            if (err) {
+                console.log(err);
+            }
+        })
+    
+        return {error: null}
+    }
+}
 
 //updateData
+const updateData = (id, dataUpdate) => {
+
+    const maravilhosaId = id;
+    const maravilhosaFound = dados.find(item => item.id == maravilhosaId); 
+    const maravilhosaIndex = dados.indexOf(maravilhosaFound);
+
+    if (maravilhosaIndex >= 0) { 
+        dados.splice(maravilhosaIndex, 1, dataUpdate);
+        fs.writeFileSync('./src/data/data.json', JSON.stringify([dados]), 'utf8', (err) => {
+            if (err) {
+                console.log(err);
+            }
+        })
+
+        return {error: null, data: selectDataById(id)}
+    } else {
+        return {error: {message: "Registro não encontrado, não foi possível fazer a alteração."}, data: null}
+    }
+}
 
 //deleteData
+const deleteData = (id) => {
+
+    const maravilhosaId = id;
+    const maravilhosaFound = dados.find(item => item.id == maravilhosaId);
+    const maravilhosaIndex = dados.indexOf(maravilhosaFound);
+
+    if (maravilhosaIndex >= 0) { 
+        dados.splice(maravilhosaIndex, 1);
+        fs.writeFileSync('./src/data/data.json', JSON.stringify([dados]), 'utf8', (err) => {
+            if (err) {
+                console.log(err);
+            }
+        })
+        return {error: null}
+    } else {
+        return {error: {message: "Registro não encontrado, não foi possível deletá-lo."}}
+    }
+}
+
+module.exports = {
+    selectAllData,
+    selectDataById,
+    insertData,
+    updateData,
+    deleteData
+}
